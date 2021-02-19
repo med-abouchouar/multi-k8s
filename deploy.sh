31f47d5adbcd37f533ac31a93bf97297e7455624
med-abouchouar <abouchouar98@gmail.com>
2021-02-19 23:10:35 +0100

    rely on SHA version in dep

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ docker build -t abouchouar/multi-client:latest -t abouchouar/multi-client:$SHA -
 docker build -t abouchouar/multi-server:latest -t abouchouar/multi-server:$SHA -f ./server/Dockerfile ./server
 docker built -t abouchouar/multi-worker:latest -t abouchouar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push abouchouar/multi-client:latest 
-docker push abouchouar/multi-server:latest
-docker push abouchouar/multi-worker:latest
+#docker push abouchouar/multi-client:latest 
+#docker push abouchouar/multi-server:latest
+#docker push abouchouar/multi-worker:latest
  
 docker push abouchouar/multi-client:$SHA 
 docker push abouchouar/multi-server:$SHA

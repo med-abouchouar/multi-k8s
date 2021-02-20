8332e7bdfcacc968b3f842966a917c440a19b83c
med-abouchouar <abouchouar98@gmail.com>
2021-02-20 02:01:00 +0100

    try to fix pg db error

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t abouchouar/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t abouchouar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker build -t abouchouar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t abouchouar/multi-client:latest  -t abouchouar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t abouchouar/multi-worker:latest  -t abouchouar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t abouchouar/multi-server:latest  -t abouchouar/multi-server:$SHA -f ./server/Dockerfile ./server
 
 
-# -t abouchouar/multi-client:latest 
-# -t abouchouar/multi-server:latest 
-# -t abouchouar/multi-worker:latest 
+# 
+# 
+# 
 
-# docker push abouchouar/multi-client:latest 
-# docker push abouchouar/multi-server:latest
-# docker push abouchouar/multi-worker:latest
+ docker push abouchouar/multi-client:latest 
+ docker push abouchouar/multi-server:latest
+ docker push abouchouar/multi-worker:latest
 
 docker push abouchouar/multi-worker:$SHA
 docker push abouchouar/multi-client:$SHA 
